7cdd141a004504b3c121b665804ac0b07eddf5c7
NimraTahir16 <63205505+NimraTahir16@users.noreply.github.com>
2022-07-08 20:27:35 +0500

    Add files via upload
    Added mad-lib replacement variables to the story

---
+++ madlibs.js
@@ -0,0 +1,5 @@
+/*let Story="Boom!! There was a massive collision. There was a loud deafening noise and dust covering the air. The dust settled and people saw there was absolute destruction. Two bogies of train had collied and there were bodies lying all around. Reporters came and started shooting while there were people asking for help and they could be saved";*/
+
+let originalStory=`Boom!! There was a massive ${userInput[0]}. There was a loud deafening ${userInput[1]}, dust covering the ${userInput[2]}. The dust settled and ${userInput[3]} saw there was absolute ${userinput[4]}. Two ${userInput[5]} of train had ${userInput[6]} and there were ${userInput[7]} lying all around. ${userInput[8]} came and started ${userInput[9]} while there were people ${userInput[10]} asking for ${userInput[11]} and they could be ${userInput[12]}`;
+
+
