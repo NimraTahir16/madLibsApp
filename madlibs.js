90186f9a6940fa79d17763f42d4e11cf89a6dfc2
NimraTahir16 <63205505+NimraTahir16@users.noreply.github.com>
2022-07-09 00:26:40 +0500

    Update madlibs.js

--- madlibs.js
+++ madlibs.js
@@ -1,5 +1,56 @@
-/*let Story="Boom!! There was a massive collision. There was a loud deafening noise and dust covering the air. The dust settled and people saw there was absolute destruction. Two bogies of train had collied and there were bodies lying all around. Reporters came and started shooting while there were people asking for help and they could be saved";*/
+<!DOCTYPE html>
+<html>
+<head>
+	<title>mad libs</title>
+</head>
+<body>
+<script type="text/javascript">
+	
 
-let originalStory=`Boom!! There was a massive ${userInput[0]}. There was a loud deafening ${userInput[1]}, dust covering the ${userInput[2]}. The dust settled and ${userInput[3]} saw there was absolute ${userinput[4]}. Two ${userInput[5]} of train had ${userInput[6]} and there were ${userInput[7]} lying all around. ${userInput[8]} came and started ${userInput[9]} while there were people ${userInput[10]} asking for ${userInput[11]} and they could be ${userInput[12]}`;
 
+  /*let Story="Boom!! There was a massive collision. There was a loud deafening noise and dust covering the air. The dust settled and people saw there was absolute destruction. Two bogies of train had collied and there were bodies lying all around. Reporters came and started shooting while there were people asking for help and they could be saved";*/
+"use strict";
 
+window.onload = () => {
+  // Total questions
+  let questionTotal = 12;
+
+  // Question Counter (which question are we on)
+  let questionCounter = 0;
+
+  // Test logic:
+  let solicitation = `Please enter a`;
+  let questionArray = [
+     window.prompt("Enter a noun that is an act"),window.prompt("Enter a noun"),window.prompt("Enter a word that is noun and verb at a same time"),window.prompt("Enter a verb / collective noun"),window.prompt("Enter a noun"),window.prompt("Enter a noun"),window.prompt("Enter a verb"),window.prompt("Enter a noun that is plural"),window.prompt("Enter a collective noun"),window.prompt("Enter a verb"),window.prompt("Enter a verb or collective noun"),window.prompt("Enter a noun"),window.prompt("Enter a verb"),
+  ];
+
+  let userInputs = [];
+  for (let i = questionTotal; i >= 0; i--) {
+    // console.log(`The "i" of our "for" loop `);
+    // console.log(i);
+    // console.log("What question (of questionArray) is accessed (using questionCounter):");
+    console.log(
+      questionArray[questionCounter] + `... (${questionTotal} questions left)`
+    );
+    userInputs.push(
+      prompt(
+        questionArray[questionCounter] + `... (${questionTotal} questions left)`
+      )
+    );
+    console.log(userInputs);
+    questionCounter++;
+   
+    questionTotal--;
+    
+  }
+  alert('All done! Ready for your totally-accurate, not-at-all confusing history of tech??');
+
+  let originalStory =`<h2>Boom!! There was a massive ${userInputs[0]}. There was a loud deafening ${userInputs[1]}, dust covering the ${userInputs[2]}. The dust settled and ${userInputs[3]} saw there was absolute ${userInputs[4]}. Two ${userInputs[5]} of train had ${userInputs[6]} and there were ${userInputs[7]} lying all around. ${userInputs[8]} came and started ${userInputs[9]} while there were people ${userInputs[10]} asking for ${userInputs[11]} and they could be ${userInputs[12]}</h2>`;
+  console.log(originalStory);
+
+  console.log(originalStory);
+  document.write(originalStory);
+};
+</script>
+</body>
+</html>
